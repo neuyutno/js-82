23d133f09da523281745b4dbf922d61b9dd65407
neuyutno <148278153+neuyutno@users.noreply.github.com>
2023-12-21 08:32:59 +0300

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,47 @@
+// 1. Создайте функцию, которая в качестве аргумента будет принимать имя, 
+// а возвращать строчку ‘Hello “аргумент функции”’.
+
+// const users = ['John', 'Ann', 'Alex', 'Max']
+
+// function checkForCopyItem(array, items) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (arra[i] === item) {
+//             return `Hello ${item}`
+//         }
+//     }
+//     return `Error`
+// }
+// console.log(checkForCopyItem(users, 'Alex'))
+
+
+
+// 2. Создайте массив чисел, далее создайте функцию, которая будет принимать 
+// в качестве аргумента массив чисел. Функция должна перебирать полученный 
+// массив и если число больше 10 - выводить это число в консоль.
+
+// const numbers = [1, 4 , 8, 13, 17]
+// function checkNumbers(array, items) {
+//     for (let i = 0; i > 10; i++)
+//     console.log(numbers)
+// }
+
+
+
+// 3. Предлагаю вам реализовать простенький калькулятор. Создайте функцию, 
+// которая будет принимать три аргумента, два числа и строку для выбора оператора. 
+// При вызове функции пользователь передает, например, (2, 3, ‘minus’), внутри 
+// функции происходит проверка на знак и в данном случае функция должна 
+// вернуть -1. То есть мы получаем разницу между первым и вторым аргументом. 
+// Запишите результат выполнения функции в переменную и выведите в консоль. 
+// Подобным образом реализуйте 4 математические операции: сложение, вычитание, 
+// умножение и деление.
+
+// function sumNumbers (firstNumbers, secondNumber) {
+//     return firstNumbers + seconndNumbers;
+//     return firstNumbers - seconndNumbers;
+//     return firstNumbers / seconndNumbers;
+//     return firstNumbers * seconndNumbers;
+// }
+// const result = sumNumbers(7,13)
+// console.log(result)
+
